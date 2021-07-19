43bc7bed9af01a95cbbea78bea3f96aad447d243
Arnon J. Kidron <45235520+arnonkidron@users.noreply.github.com>
2021-07-19 11:01:57 +0300

    Start

---
+++ main.py
@@ -0,0 +1,17 @@
+import polyscope as ps
+
+# initialize
+ps.set_program_name("Flip Geodesics Implementation for 236329 GDP")
+ps.set_verbosity(2)
+ps.set_use_prefs_file(False)
+ps.set_errors_throw_exceptions(True)
+ps.init()
+
+# Build visualizations, here or in distant code
+# ...
+# ...
+# ...
+
+# Pass control flow to polyscope, displaying the interactive window.
+# Function will return when user closes the window.
+ps.show()
